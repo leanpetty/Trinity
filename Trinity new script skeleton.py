2622dc58cf08732b6c0f79642136e89b6ad97c2a
Darquesse27 <acbirt1@gmail.com>
2016-04-20 14:18:16 +0700

    Added files via upload

---
+++ Trinity new script skeleton.py
@@ -0,0 +1,79 @@
+import time
+import random
+
+def introspeech():
+    #intro
+
+def clanchoice():
+    #clanchoice
+
+def initialsalesman():
+    #salesman
+
+def joinclan():
+    #joinclan
+
+def intro():
+    introspeech()
+    clanchoice()
+    salesman()
+    joinclan()
+
+def trentinitialproblem():
+    #tip
+
+def mageinitialproblem():
+    #mip
+
+def warriorinitialproblem():
+    #wip
+
+def archerinitialproblem():
+    #aip
+
+def trentlife():
+    #trentlife
+
+def magelife():
+    #magelife
+
+def warriorlife():
+    #warriorlife
+
+def archerlife():
+    #archerlife
+
+def trentrand1():
+    #random trentlife
+
+def magerand1():
+    #random magelife
+
+def warriorrand1():
+    #random warriorlife
+
+def archerrand1():
+    #random archerlifex
+
+def trentgame():
+    trentinitialproblem()
+    trentrand1()
+
+def magegame():
+    mageinitialproblem()
+    magerand1()
+
+def warriorgame():
+    warriorinitialproblem()
+    warriorrand1()
+
+def archergame():
+    archerinitialproblem()
+    archerrand1()
+
+def clangame():
+    #if/elif from clanchoice()
+
+def trinity():
+    intro()
+    clangame()
