19dcce9fc0c240b19ad763552ccd1269bb375890
Darquesse27 <acbirt1@gmail.com>
2016-04-20 14:25:32 +0700

    Update Trinity new script skeleton.py

--- Trinity new script skeleton.py
+++ Trinity new script skeleton.py
@@ -1,6 +1,64 @@
 import time
 import random
 
+w_steelsword = "Steel Sword"
+w_claws = "Claws" 
+w_orcaxe = "Orc Axe"
+w_bludgeon = "Bludgeon"
+w_hatchet = "Hatchet"
+w_scimitar = "Scimitar"
+w_hammer = "Hammer"
+w_pike = "Chinese Pike"
+w_stick = "Walking Stick"
+w_trainingsword = "Training Sword"
+
+w_mallet = "Granite Mallet"
+w_mandibles = "Mandibles"
+w_gladius = "Golden Gladius"
+w_demonblade = "DemonBlade"
+w_venomblade = "VenomBlade"
+w_gemaxe = "Gem Axe"
+w_cudgel = "Cudgel"
+w_staff = "Blue Crystal Staff"
+w_halberd = "Halberd"
+w_doubleaxe = "Double-Sided Axe"
+
+w_diamondspark = "Diamond Spark"
+w_zweireaper = "Zweireaper"
+w_runeblade = "Runic Blade"
+w_greatsword = "Elven Greatsword"
+w_gemcrusher = "Gemcrusher"
+w_divinereach = "Divine Reach"
+w_hammeroflight = "Hammer Of Light"
+w_magmasword = "Magma Sword"
+w_demoneye = "Demon Eye"
+w_katar = "Katar"
+w_katana = "Nethersteel Katana"
+w_toothcrusher = "Tooth Crusher"
+w_runicaxe = "Runic Axe"
+w_lunar = "Lunar Relic"
+
+w_rubythorn = "Ruby Thorn"
+w_drakefang = "Drake Fang Giantbone"
+w_voidtwig = "Foul Flower Void Twig"
+w_broccomace = "Rune Mace Broccomace"
+w_frostbite = "Ice Shard Frostbite"
+w_enderfist = "Archon Blade Enderfist"
+w_voidedge = "Void Edge"
+w_gemini = "Gemini"
+w_felflame = "Felflame"
+w_armblade = "Armblade"
+w_amaranth = "Amaranth"
+
+
+w_jahestirr = "Goaer's Recurve"
+w_ara = "All-Powerful Rune"
+w_esmyau = "Spellbreaker's Greatshield"
+w_umisr = "Ominous Warblade of the Unbreakable"
+w_trillini = "Hellreaver"
+w_lenicara = "Armageddon"
+w_goaer = "Triple Pronged Mechanical Sword"
+
 def introspeech():
     #intro
 
